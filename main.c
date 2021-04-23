cb3b2916646fea228ff54e71155944f3b6ce0507
Nikhil <agrawalnikhil12345@gmail.com>
2021-04-23 18:41:05 +0530

    Added 3 files which make tree

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "tsl.h"
+
+int main()
+{
+int n ;
+scanf("%d",&n);    // this can be shifted into .c also
+
+Node tree;
+Node* parentptr;
+    parentptr = (Node*)malloc(sizeof(Node)*n);
+
+tree = create_Tree(n,parentptr); 
+
+print_tree(parentptr,n);
+return 0; 
+
+}
